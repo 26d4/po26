8d4020f79c51939479db3382e9d92945301ce9f2
Witold Węglarz <wrglarz25@gmail.com>
2026-03-28 01:19:51 +0100

    2: product entity

--- 2/entrypoint.sh
+++ 2/entrypoint.sh
@@ -4,4 +4,12 @@ cd "$(dirname "$(readlink -e "$0")")"
 
 #export APP_ENV=prod
 
+#workaround for doctrine's bullshit
+export DB_FILE=$(bin/console debug:config --resolve-env doctrine | grep url: | sed s,^.*sqlite://,, | tr -d "'")
+
+if ! [[ -e "$DB_FILE" ]]; then
+	sqlite3 "$DB_FILE" ""
+	php bin/console doctrine:schema:update --force
+fi
+
 exec bash
