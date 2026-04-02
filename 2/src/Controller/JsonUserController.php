03a68164bdb58a20e730ca50f59b0a8ef2295893
Witold Węglarz <wrglarz25@gmail.com>
2026-04-02 23:16:18 +0200

    2: more entities and controllers

---
+++ 2/src/Controller/JsonUserController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+#[Route('/api/user')]
+final class JsonUserController extends AbstractController
+{
+	#[Route(name: 'app_json_user_index', methods: ['GET'], format: 'json')]
+	public function index(UserRepository $userRepository): Response
+	{
+		return $this->json($userRepository->findAll());
+	}
+
+	#[Route(name: 'app_json_user_new', methods: ['POST'], format: 'json')]
+	public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, ValidatorInterface $validator): Response
+	{
+		if ('json' !== $request->getContentTypeFormat()) {
+			return $this->json(['message' => 'Unsupported content format'], 400);
+		}
+
+		$jsonData = $request->getContent();
+		$user = $serializer->deserialize($jsonData, User::class, 'json');
+		
+		$errors = $validator->validate($user);
+		if (count($errors) > 0) {
+			return $this->json(['message' => (string) $errors], 400);
+		}
+		
+		$entityManager->persist($user);
+		$entityManager->flush();
+		
+		return $this->json($user, status: Response::HTTP_CREATED);
+	}
+
+	#[Route('/{id}', name: 'app_json_user_show', methods: ['GET'], format: 'json')]
+	public function show(User $user): Response
+	{
+		return $this->json($user);
+	}
+
+	#[Route('/{id}', name: 'app_json_user_edit', methods: ['PATCH', 'PUT'], format: 'json')]
+	public function edit(Request $request, User $user, EntityManagerInterface $entityManager, SerializerInterface $serializer, ValidatorInterface $validator): Response
+	{
+		if ('json' !== $request->getContentTypeFormat()) {
+			return $this->json(['message' => 'Unsupported content format'], 400);
+		}
+
+		$jsonData = $request->getContent();
+		$userMod = $serializer->deserialize($jsonData, User::class, 'json');
+		
+		$errors = $validator->validate($userMod);
+		if (count($errors) > 0) {
+			return $this->json(['message' => (string) $errors], 400);
+		}
+		
+		if ($request->getMethod() === 'PUT') {
+			$diff = array_diff($entityManager->getClassMetadata(User::class)->getFieldNames(), array_keys(json_decode($jsonData, true)));
+			$diff = array_diff($diff, array('id'));
+			if (count($diff) > 0) {
+				return $this->json(['message' => "Missing fields", 'missing_fields' => array_values($diff)], 400);
+			}
+		}
+		
+		$serializer->deserialize($jsonData, user::class, 'json', ['object_to_populate' => $user]);
+		$entityManager->flush();
+		
+		return $this->json($user);
+	}
+
+	#[Route('/{id}', name: 'app_json_user_delete', methods: ['DELETE'], format: 'json')]
+	public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+	{
+		if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+			$entityManager->remove($user);
+			$entityManager->flush();
+		}
+
+		return $this->json(null, Response::HTTP_NO_CONTENT);
+	}
+}
---
+++ 2/src/Entity/Article.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Clock\DatePoint;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    private ?User $author = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+    
+    #[ORM\PrePersist]
+    public function setCreatedAtAutomatically(): void
+    {
+        if ($this->getCreatedAt() === null) {
+            $this->setCreatedAt(new DatePoint());
+        }
+    }
+
+	#[ORM\PrePersist]
+    #[ORM\PreUpdate]
+    public function setUpdatedAtAutomatically(): void
+    {
+        $this->setUpdatedAt(new DatePoint());
+    }
+}
