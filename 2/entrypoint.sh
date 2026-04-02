76f8830bd34f2819b6838d919fbbf693f7a76227
Witold Węglarz <wrglarz25@gmail.com>
2026-04-02 19:52:12 +0200

    2: better container

--- 2/entrypoint.sh
+++ 2/entrypoint.sh
@@ -10,10 +10,10 @@ export DB_FILE=$(bin/console debug:config --resolve-env doctrine | grep url: | s
 if ! [[ -e "$DB_FILE" ]]; then
 	sqlite3 "$DB_FILE" ""
 	php bin/console doctrine:schema:update --force
-else
-	if [[ -n "$(php bin/console make:migration -q)" ]]; then
-		php bin/console doctrine:migrations:migrate
-	fi
+#else
+#	if [[ -n "$(php bin/console make:migration -q)" ]]; then
+#		php bin/console doctrine:migrations:migrate
+#	fi
 fi
 
 #symfony serve
