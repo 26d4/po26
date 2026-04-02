f92441c9e7de913eac8efb002a7ee56cdddcdd21
Witold Węglarz <wrglarz25@gmail.com>
2026-04-03 01:39:25 +0200

    2: update entrypoint

--- 2/entrypoint.sh
+++ 2/entrypoint.sh
@@ -2,12 +2,18 @@
 
 cd "$(dirname "$(readlink -e "$0")")"
 
-#export APP_ENV=prod
+source .env
+source .env.local
+source .env."$APP_ENV"
+source .env."$APP_ENV".local
+
+echo "APP ENV = $APP_ENV"
 
 #workaround for doctrine's bullshit
 export DB_FILE=$(bin/console debug:config --resolve-env doctrine | grep url: | sed s,^.*sqlite://,, | tr -d "'")
 
-if ! [[ -e "$DB_FILE" ]]; then
+if ! [[ -e "$DB_FILE" ]] || [[ "$APP_ENV" = test ]]; then
+	if [[ "$APP_ENV" = test ]]; then rm "$DB_FILE"; fi
 	sqlite3 "$DB_FILE" ""
 	php bin/console doctrine:schema:update --force
 #else
@@ -16,5 +22,5 @@ if ! [[ -e "$DB_FILE" ]]; then
 #	fi
 fi
 
-#symfony serve
+symfony serve
 bash
