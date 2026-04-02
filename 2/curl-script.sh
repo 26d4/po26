3d66aa7ca25a85470bc47659c9be56e2314f4f5d
Witold Węglarz <wrglarz25@gmail.com>
2026-04-02 19:52:48 +0200

    2: actually improve curl script (oops)

--- 2/curl-script.sh
+++ 2/curl-script.sh
@@ -4,8 +4,8 @@ set -e
 
 announce() { local -; set -x; "$@"; }
 
-jdiff() {
-	diff <(jq --sort-keys . "$1") <(jq --sort-keys . "$2")
+jqdiff() {
+	diff <(jq --sort-keys . <<< "$1") <(jq --sort-keys . <<< "$2")
 }
 
 tc() {
@@ -23,36 +23,34 @@ ID=$(echo "$RESPONSE" | jq '.id')
 
 echo "$RESPONSE    -->  id=$ID"
 
-sleep 3
+sleep 1
 
 tc "Show product" "$(announce curl --no-progress-meter http://localhost:8000/api/product/"$ID")" "{\"id\":$ID,\"name\":\"xyz\",\"price\":2999}"
 echo
-sleep 3
+sleep 1
 
 echo "Edit product"
 announce curl --no-progress-meter --header "Content-Type: application/json" --request PATCH --data '{"price":4950}' http://localhost:8000/api/product/"$ID"
 echo
-sleep 3
+sleep 1
 
-echo "Show product again"
-announce curl http://localhost:8000/api/product/"$ID"
+tc "Show product again" "$(announce curl http://localhost:8000/api/product/"$ID")" "{\"id\":$ID,\"name\":\"xyz\",\"price\":4950}"
 echo
-sleep 3
+sleep 1
 
-echo "Replace product"
-announce curl --no-progress-meter --header "Content-Type: application/json" --request PUT --data '{"name":"zyx"}' http://localhost:8000/api/product/"$ID"
+tc "Replace product (Bad put)" "$(announce curl --no-progress-meter --header "Content-Type: application/json" --request PUT --data '{"name":"zyx"}' http://localhost:8000/api/product/"$ID")" "{\"message\": \"Missing fields: price\"}"
 echo
-sleep 3
+sleep 1
 
 echo "Show product list"
 announce curl --no-progress-meter http://localhost:8000/api/product
 echo
-sleep 3
+sleep 1
 
 echo "Delete product"
 announce curl --no-progress-meter --request DELETE http://localhost:8000/api/product/"$ID"
 echo
-sleep 3
+sleep 1
 
 echo "Show product list again"
 announce curl --no-progress-meter http://localhost:8000/api/product
