843ac54b7dbc3373151e33485e7d4b5d7b75f23d
Witold Węglarz <wrglarz25@gmail.com>
2026-04-10 13:47:15 +0200

    4: call weather API

--- 4/server.go
+++ 4/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"io"
 
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
@@ -12,7 +13,20 @@ func main() {
 	e.Use(middleware.RequestLogger())
 
 	e.GET("/", func(c *echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		response, err := http.Get("http://api.open-meteo.com/v1/forecast?latitude=50.0614&longitude=19.9366&timezone=auto&current=temperature_2m,precipitation,weather_code,wind_speed_10m")
+
+		if err != nil {
+		    e.Logger.Error(err.Error())
+		    return echo.NewHTTPError(http.StatusBadGateway, "Bad API response")
+		}
+
+		responseData, err := io.ReadAll(response.Body)
+		if err != nil {
+		    e.Logger.Error(err.Error())
+		    return echo.NewHTTPError(http.StatusBadGateway, "Failed to read API response")
+		}
+
+		return c.JSONBlob(http.StatusOK, responseData)
 	})
 
 	if err := e.Start(":1323"); err != nil {
